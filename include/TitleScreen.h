2da31b404fc395eff419b6382fd9ac6edb67a7f8
danelbro <dan.elbro@gmail.com>
2024-10-31 20:26:59 +0000

    Add TitleScreen files

---
+++ include/TitleScreen.h
@@ -0,0 +1,33 @@
+// -*- C++ -*- //
+/// @file A Stage that displays a title screen and, depending on input, either
+/// quits or goes into the main game stage
+
+#pragma once
+
+#include <array>
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <utl_Box.hpp>
+#include <utl_SDLInterface.hpp>
+#include <utl_Stage.hpp>
+#include <vector>
+#include <utl_Entity.hpp>
+
+/// A Stage that displays a title screen and, depending on input, either quits
+/// or goes into the main game stage
+class TitleScreen : public utl::Stage {
+public:
+    /// Constructs a TitleScreen
+    /// @param screen
+    TitleScreen(utl::Box& screen, uint32_t windowID, utl::Renderer& renderer);
+
+    std::string
+    handle_input(double t, double dt,
+                 std::array<bool, utl::KeyFlag::K_TOTAL>& keyState) override;
+    std::string update(double t, double dt) override;
+    void render(double t, double dt) override;
+
+private:
+    std::vector<std::unique_ptr<utl::Entity>> entities;
+};
