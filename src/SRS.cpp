7818a9d108db387ee4ebd4eadd5cba4e85bad513
danelbro <dan.elbro@gmail.com>
2025-08-15 15:27:14 +0100

    Simplify SRS test map

--- src/SRS.cpp
+++
@@ -1,8 +0,0 @@
-#include "SRS.h"
-
-bool operator==(const SRSKey& left, const SRSKey& right)
-{
-    return left.test_ == right.test_ && left.type_ == right.type_
-           && left.currentRotation_ == right.currentRotation_
-           && left.newRotation_ == right.currentRotation_;
-}
