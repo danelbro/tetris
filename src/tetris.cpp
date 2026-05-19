e00f5eb3337850624d9b3d9b9cf0544802fa74cb
danelbro <dan.elbro@gmail.com>
2026-05-19 15:37:08 +0100

    Handle SdlExceptions

--- src/tetris.cpp
+++ src/tetris.cpp
@@ -1,12 +1,16 @@
 #include "TetrisApp.h"
 
+#include <utl_SDLInterface.hpp>
 #include <SDL3/SDL_main.h>
 
 int main(int, char**)
 {
-    {
+    try {
         TetrisApp tetris{};
         tetris.run();
+    }
+    catch (utl::SdlException&) {
+        return 1;
     }
 
     return 0;
